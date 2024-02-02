997e23b520ed4f611715e969ef295ea0c46fc303
VineethReddy18183 <118506794+VineethReddy18183@users.noreply.github.com>
2024-02-02 18:19:48 +0530

    Add files via upload

---
+++ goodreadsTwoTables/src/main/java/com/example/goodreadsTwoTables/model/Publisher.java
@@ -0,0 +1,41 @@
+package com.example.goodreadsTwoTables.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "publisher")
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "publisherid")
+    private int publisherId;
+
+    @Column(name = "publishername")
+    private String publisherName;
+
+    public Publisher() {
+    }
+
+    public int getPublisherId() {
+        return publisherId;
+    }
+
+    public void setPublisherId(int publisherId) {
+        this.publisherId = publisherId;
+    }
+
+    public String getPublisherName() {
+        return publisherName;
+    }
+
+    public void setPublisherName(String publisherName) {
+        this.publisherName = publisherName;
+    }
+
+}
