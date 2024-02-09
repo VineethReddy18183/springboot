84c39b27c93f886455fc335b79b14645d0325500
VineethReddy18183 <118506794+VineethReddy18183@users.noreply.github.com>
2024-02-09 11:50:10 +0530

    Add files via upload
    Spring sms started

---
+++ springSms/src/test/java/com/example/springSms/SpringSmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springSms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
